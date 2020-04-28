cee4a4d628671f338cb6643ba9ab208b9bf14c0d
Sg711 <steven.guidat.sg711@gmail.com>
2020-04-28 12:24:56 +0200

    else finish

--- js/script.js
+++ js/script.js
@@ -8,5 +8,34 @@ $('form').submit(function()){
         $('#view').html('<p class="red">Champ vide !</p>');
     } else {
 
+        let cityTable = $('<thead><tr><th>Nom</th><th>Code Postal</th><th>Nombre d\'habitants</th><th>Département</th></tr></thead>');
+
+        data.forEach(function(city){
+
+            let newCity = $('<tr></tr>')
+
+            let cityName = $('<td></td>');
+            cityName.text(city.nom);
+
+            let cityZipCode = $('<td></td>');
+            cityZipCode.text(city.zipCode);
+
+            let cityPeopleNumber = $('<td></td>');
+            cityPeopleNumber.text(city.peopleNumber);
+
+            let cityDepartment = $('<td></td>');
+            cityDepartment.text(city.department);
+
+            newCity.append(cityName);
+            newCity.append(cityZipCode);
+            newCity.append(cityPeopleNumber);
+            newCity.append(cityDepartment);
+
+            cityTable.find('tbody').append(newCity);
+            console.log(city.peopleNumber);
+        });
+
+        $('table').html(cityTable);
+
     }
 }
